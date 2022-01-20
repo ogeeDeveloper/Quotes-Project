b5b26575cc6e57ebec0b2076ff0d702a0a3ab66e
ogeeDeveloper <omarogrant202@hotmail.com>
2022-01-20 09:35:36 -0500

    IMplemented Query Params for sorting quotes. Key takeaway are UseLocation, useNavigate hook and also the URLSearchParms object from the browser

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,48 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
+
+// Sort Quotes helper Function
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
 
 const QuoteList = (props) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // console.log(location);\
+  // Get the key in the search param
+  const queryParams = new URLSearchParams(location.search);
+
+  // Create a helper constant
+  const isSortingAscending = queryParams.get("sort") === "asc"; // Get us value stored in sort param
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+  const changeSortHandler = () => {
+    // Change how we sort our quotes
+    navigate(`/quotes?sort=${isSortingAscending ? "desc" : "asc"}`);
+    // navigate("/quotes?sort=" + (isSortingAscending ? "desc" : "asc"));
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortHandler}>
+          Sort {isSortingAscending ? "Descending" : "Ascending"}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
