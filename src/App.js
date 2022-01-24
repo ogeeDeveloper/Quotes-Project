41e7ae38e581cc37dd2503a01e07cab5b8105ede
ogeeDeveloper <omarogrant202@hotmail.com>
2022-01-23 19:07:21 -0500

    Added the lazy loading functionality into app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Comments from "./components/comments/Comments";
-import AllQuotes from "./pages/AllQuotes";
 import QuoteDetail from "./pages/QuoteDetail";
-import NewQuote from "./pages/NewQuote";
 import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const AllQuotes = lazy(() => import("./pages/AllQuotes"));
+const NewQuote = lazy(() => import("./pages/NewQuote"));
 
 function App() {
   // Handling Redirects in react-router-dom v6
@@ -16,15 +19,23 @@ function App() {
 
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/quotes" />} />
-        <Route path="/quotes" element={<AllQuotes />} exact />
-        <Route path="/quotes/:quoteId" element={<QuoteDetail />}>
-          <Route path={`/quotes/:quoteId/comments`} element={<Comments />} />
-        </Route>
-        <Route path="/add-quote" element={<NewQuote />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/quotes" />} />
+          <Route path="/quotes" element={<AllQuotes />} exact />
+          <Route path="/quotes/:quoteId" element={<QuoteDetail />}>
+            <Route path={`/quotes/:quoteId/comments`} element={<Comments />} />
+          </Route>
+          <Route path="/add-quote" element={<NewQuote />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
